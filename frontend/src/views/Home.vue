79427b9820cb3cf045e0b8306355df4da208d861
resulyrt93 <resulyrt93@gmail.com>
2021-05-01 20:00:19 +0300

    # frontend with bootstrap initialized

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <button type="button" class="btn btn-primary">Secondary</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
